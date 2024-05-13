15c269923c204c8af4b19c5885a8ce78c5b2f1db
Folavin17 <szymon.mendakiewicz@gmail.com>
2024-05-13 13:54:12 +0200

    chore: Remove commented out code and unused module imports

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::sync::mpsc;
-// mod serial_connection;
+// use std::sync::mpsc;
+mod serial_connection;
 mod detect_color;
-pub mod serial_connection_con;
+// pub mod serial_connection_con;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    serial_connection_con::initialize_serial(tx);
-    for i in 0..10 {
-        println!("Main thread is running {}", i);
-        let color_values = rx.recv().unwrap();
-        print!("{:?}", detect_color::logic(color_values.0, color_values.1, color_values.2));
-        std::thread::sleep(std::time::Duration::from_secs(1));
-    }
+    // let (tx, rx) = mpsc::channel();
+    // serial_connection_con::initialize_serial(tx);
+    // for i in 0..10 {
+    //     println!("Main thread is running {}", i);
+    //     let color_values = rx.recv().unwrap();
+    //     print!("{:?}", detect_color::logic(color_values.0, color_values.1, color_values.2));
+    //     std::thread::sleep(std::time::Duration::from_secs(1));
     
-    // let color_values = serial_connection::serial_connect();
+    
+    let color_values = serial_connection::serial_connect();
+    print!("{:?}", detect_color::logic(color_values.0, color_values.1, color_values.2));
 }
