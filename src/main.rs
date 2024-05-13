274450bdd10e0459491a845133940737cb9dd6c8
Folavin17 <szymon.mendakiewicz@gmail.com>
2024-05-13 12:55:14 +0200

    Minor tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-mod serial_connection;
+use std::sync::mpsc;
+// mod serial_connection;
 mod detect_color;
+pub mod serial_connection_con;
 
 fn main() {
-    let color_values = serial_connection::serial_connect();
-    print!("{:?}", detect_color::logic(color_values.0, color_values.1, color_values.2));
+    let (tx, rx) = mpsc::channel();
+    serial_connection_con::initialize_serial(tx);
+    for i in 0..10 {
+        println!("Main thread is running {}", i);
+        let color_values = rx.recv().unwrap();
+        print!("{:?}", detect_color::logic(color_values.0, color_values.1, color_values.2));
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+    
+    // let color_values = serial_connection::serial_connect();
 }
