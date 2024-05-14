11d523e0e85d8792aa2bb09fc176a55bb1ea982d
Folavin17 <szymon.mendakiewicz@gmail.com>
2024-05-14 14:18:39 +0200

    First implementation of state maschine

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,90 @@
 mod serial_connection_con;
 mod detect_color;
-use std::sync::mpsc;
+mod motors;
+use std::{sync::mpsc, thread::current};
+
+#[derive(Clone, Copy)]
+enum State {
+    Ready,
+    Detecting,
+    Positioning,
+    Analyzing,
+    Discarding,
+    Sorting,
+    Error,
+    Paused,
+}
+
+
+enum Event {
+    PowerOn,
+    DiscDetected,
+    DiscPositioned,
+    DiscAnalyzed { needed: bool },
+    Discarded,
+    Sorted,
+    OperationError,
+    Reset,
+    Pause,
+    Resume,
+}
+
+fn next_state(current_state: State, event: Event) -> State {
+    use State::*;
+    use Event::*;
+
+    match (current_state, event) {
+        (Ready, PowerOn) | (Ready, Resume) => Detecting,
+        (Detecting, DiscDetected) => Positioning,
+        (Positioning, DiscPositioned) => Analyzing,
+        (Analyzing, DiscAnalyzed { needed }) if needed => Sorting,
+        (Analyzing, DiscAnalyzed { needed: false }) => Discarding,
+        (Discarding, Discarded) | (Sorting, Sorted) => Ready,
+        (_, OperationError) | (_, Pause) => Error,
+        (Error, Reset) => Ready,
+        _ => current_state,
+    }
+}
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    serial_connection_con::initialize_serial(tx); // Start the serial connection in a separate thread
+    // COLOR detection initialization
+    let (tx_color, rx_color) = mpsc::channel();
+    serial_connection_con::initialize_serial(tx_color); // Start the serial connection in a separate thread
     std::thread::sleep(std::time::Duration::from_secs(3)); // Wait for the serial connection to initialize
-    let color_values = serial_connection_con::get_nwst_color(&rx);
-    println!("{:?}", detect_color::logic(color_values.0, color_values.1, color_values.2));
+    // DISTANCE sensor initialization
+    // TODO: Add distance sensor initialization here
+
+    // Motor initialization
+    // TODO: Add motor initialization here
+
+    let mut current_state = State::Ready;
+    loop {
+        match current_state {
+            State::Ready => {
+
+            },
+            State::Detecting => {
+
+            },
+            State::Positioning => {
+                
+            },
+            State::Analyzing => {
+                
+            },
+            State::Discarding => {
+                
+            },
+            State::Sorting => {
+                
+            },
+            State::Error => {
+                
+            },
+            State::Paused => {
+                
+            },
+        }
+
+    }
 }
