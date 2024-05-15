37380a2ed781bd9bbb53c4fdba0814d73743ece0
Folavin17 <szymon.mendakiewicz@gmail.com>
2024-05-15 12:52:25 +0200

    Implemented new functions

--- src/sorting.rs
+++ src/sorting.rs
@@ -16,40 +16,45 @@ Black disk: 1
 Other/unknown: 2 - the below code will not be called if disk is other or unknown
 */
 
-pub fn sort_discs(bins: (Vec<i32>, Vec<i32>, Vec<i32>), pattern: i32, disc: i32) -> (i32, bool) {
-    let bin1 = bins.0;
-    let bin2 = bins.1;
-    let bin3 = bins.2;
-    let mut needed = false;
+pub fn sort_disc(bins: &(Vec<i32>, Vec<i32>, Vec<i32>), pattern: &i32, disc: &i32) -> i32 {
+    let (next_bin1, next_bin2, next_bin3) = get_next_needed(bins, pattern);
 
-    // find the next disk needed for each bin
-    // TODO: Get the next disk needed for each bin from the chosen data structure
-    // set 2 if no more needed (bin full)
-    // Placeholder
-    let next_bin1 = 0;
-    let next_bin2 = 0;
-    let next_bin3 = 0;
-
-    // check if the disk is needed
-    if !(disc == next_bin1 || disc == next_bin2 || disc == next_bin3) {
-        return (0, false);
-    }
-    
     // sort the disc
     // try the first bin
-    if disc == next_bin1 {
-        return (1, true)
+    if *disc == next_bin1 {
+        return 1;
     } else {
         // try the second bin
-        if disc == next_bin2 {
-            return (2, true)
+        if *disc == next_bin2 {
+            return 2;
         } else {
             // try the third bin
-            if disc == next_bin3 {
-                return (3, true)
+            if *disc == next_bin3 {
+                return 3;
             }
         }
     }
     // if error in sorting (should not happen)
-    (-1, false)
+    return -1
+}
+
+pub fn check_needed(bins: &(Vec<i32>, Vec<i32>, Vec<i32>), pattern: &i32, disc: &i32) -> bool {
+    let (next_bin1, next_bin2, next_bin3) = get_next_needed(bins, pattern);
+
+    if !(*disc == next_bin1 || *disc == next_bin2 || *disc == next_bin3) {
+        return false;
+    }
+    return true;
+}
+
+fn get_next_needed(bins: &(Vec<i32>, Vec<i32>, Vec<i32>), pattern: &i32) -> (i32, i32, i32) {
+    let bin1 = &bins.0;
+    let bin2 = &bins.1;
+    let bin3 = &bins.2;
+
+    // find the next disk needed for each bin
+    // TODO: implement
+    
+    // Placeholder
+    return (0, 1, 0)
 }
