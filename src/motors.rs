32db2371c2fbedc76fd2188b72f93eb7629542af
Folavin17 <szymon.mendakiewicz@gmail.com>
2024-05-17 19:31:52 +0200

    Soring arms code refactoring

--- src/motors.rs
+++ src/motors.rs
@@ -1,7 +1,7 @@
 /*
 Placeholder until proper implementation
 */
-
+use log::{error, info};
 
 // CONVEYOR
 pub fn start_conveyor(speed: i32) -> () {
@@ -15,45 +15,35 @@ pub fn stop_conveyor() -> () {
 }
 
 // SORTING ARMS
+/// direction: 0 - left, 1 - right
 pub fn move_sort_arm_1(direction: i32) -> () {
     println!("Moving motor 1 to direction: {}", direction);
     // TODO: Implement motor movement logic here
 }
 
+/// direction: 0 - left, 1 - right
 pub fn move_sort_arm_2(direction: i32) -> () {
     println!("Moving motor 2 to direction: {}", direction);
     // TODO: Implement motor movement logic here
 }
 
-/*
-Sort to bin 1
-*/
-pub fn sort_arm_to_1() -> () {
-    move_sort_arm_1(1); // Move motor 1 to right
-    // TODO: check movement time
-    std::thread::sleep(std::time::Duration::from_secs(1)); // wait until movement is complete
-}
-/*
-Sort to bin 2
-*/
-pub fn sort_arm_to_2() -> () {
-    move_sort_arm_1(0); // Move motor 1 to left
-    move_sort_arm_2(1); // Move motor 2 to right
-    // TODO: check movement time
-    std::thread::sleep(std::time::Duration::from_secs(1)); // wait until movement is complete
-}
-/*
-Sort to bin 3
-*/
-pub fn sort_arm_to_3() -> () {
-    move_sort_arm_1(0); // Move motor 1 to left
-    move_sort_arm_2(0); // Move motor 2 to left
-    // TODO: check movement time
-    std::thread::sleep(std::time::Duration::from_secs(1)); // wait until movement is complete
+/// Sets the sorting arms to the specified bin
+pub fn sort_arm(bin: i32) -> () {
+    match bin {
+        1 => move_sort_arm_1(0), // Move arm 1 to left,
+        2 => {
+            move_sort_arm_1(0); // Move arm 1 to left
+            move_sort_arm_2(1); // Move arm 2 to right
+        },
+        3 => {
+            move_sort_arm_1(0); // Move arm 1 to left
+            move_sort_arm_2(0); // Move arm 2 to left
+        },
+        _ => error!("Sorting error: invalid bin number"),
+    }
 }
 
 // DISCARD MOTOR
 pub fn discard_item() -> () {
     println!("Discarding item");
-    std::thread::sleep(std::time::Duration::from_secs(1)); // wait until movement is complete
 }
